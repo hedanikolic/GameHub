c5c038c69d4cd3206e6c540e154384f80ac638b8
hedanikolic <hedanikolic23@gmail.com>
2024-01-23 15:05:34 +0100

    Create RMA app

---
+++ RMA-app/RMA-app/RMA_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMA_appApp.swift
+//  RMA-app
+//
+//  Created by student on 23.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct RMA_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
